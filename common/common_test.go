feea82862326efe0951b5e384b2405949db38326
Matt J. Wiater <matt@brightpixel.com>
2023-04-21 23:45:15 +0000

    working on better testing

--- common/common_test.go
+++ common/common_test.go
@@ -30,7 +30,6 @@ func TestGetAttr(t *testing.T) {
 
 	job1 := structs.Job{JobNumber: 1, Id: uuid, JobName: "EmptySleepJob", Data: ""}
 	jobResult1 := structs.JobResult{WorkerID: uuid, Job: job1, NumberOfJobs: 16, JobTimer: float64(1), JobMemAlloc: float32(1), JobName: "EmptySleepJob", Status: ""}
-	jobResult2 := structs.JobResult{WorkerID: uuid, Job: job1, NumberOfJobs: 16, JobTimer: 1, JobMemAlloc: float32(1), JobName: "EmptySleepJob", Status: ""}
 
 	t.Run("success", func(t *testing.T) {
 		var tests = []struct {
@@ -39,36 +38,30 @@ func TestGetAttr(t *testing.T) {
 			input2   string
 			want     reflect.Value
 		}{
-			// the table itself
-			{"equal", &jobResult1, "JobTimer", testJobResultAttr},
+			{"values are equal", &jobResult1, "JobTimer", testJobResultAttr},
 		}
 
 		for _, tt := range tests {
 			t.Run(tt.testName, func(t *testing.T) {
 				ans, _ := common.GetAttr(tt.input1, tt.input2)
-				fmt.Println("EQUAL", ans, tt.want)
 				assert.EqualValues(t, fmt.Sprintln(ans), fmt.Sprintln(tt.want))
 			})
 		}
 	})
 
-	t.Run("failure", func(t *testing.T) {
+	t.Run("calls non-existent struct field", func(t *testing.T) {
 		var tests = []struct {
 			testName string
 			input1   *structs.JobResult
 			input2   string
 			want     reflect.Value
 		}{
-			// the table itself
-			{"error", &jobResult2, "JobTimer2", testJobResultAttr},
+			{"error", &jobResult1, "JobTimer2", testJobResultAttr},
 		}
 
 		for _, tt := range tests {
 			t.Run(tt.testName, func(t *testing.T) {
-				ans, err := common.GetAttr(tt.input1, tt.input2)
-
-				fmt.Println("ERROR", ans, tt.want)
-
+				_, err := common.GetAttr(tt.input1, tt.input2)
 				assert.Error(t, err)
 			})
 		}
